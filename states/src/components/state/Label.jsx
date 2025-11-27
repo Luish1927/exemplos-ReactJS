e38a51e73716324aedc460fcabeb17d5b1695a38
LuisH <mr.luishenriquegd@icloud.com>
2025-11-27 12:58:21 -0300

    first commit

---
+++ states/src/components/state/Label.jsx
@@ -0,0 +1,11 @@
+import React, {useState} from 'react';
+
+export default function Label({ texto }) {
+  const [valor, setValor] = useState(texto);
+
+  return (
+    <>
+      {valor}
+    </>
+  );
+}
